f5e4947f519016319112cf1684ebf4809a0a2a76
Alice Gituser <alice@ctg.co.nz>
2016-10-09 11:20:23 +1300

    Create form body

--- JavaFX Demo/src/nz/co/ctg/javafx/OptionsPane.java
+++ JavaFX Demo/src/nz/co/ctg/javafx/OptionsPane.java
@@ -2,11 +2,13 @@ package nz.co.ctg.javafx;
 
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 
 public class OptionsPane extends BorderPane {
   
   public OptionsPane(String title) {
     setTop(new Label(title));
+    setCenter(new GridPane());
   }
   
 }
