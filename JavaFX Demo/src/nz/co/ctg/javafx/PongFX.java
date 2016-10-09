eb5794ac34559c34c613fe379d0d6b357900822f
Alice Gituser <alice@ctg.co.nz>
2016-10-09 14:03:11 +1300

    1: Write JavaFX Demo
    Task-Url: http://github.com/ctgnz/jug/issues/issues/1

--- JavaFX Demo/src/nz/co/ctg/javafx/PongFX.java
+++ JavaFX Demo/src/nz/co/ctg/javafx/PongFX.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class DemoApplication extends Application {
+public class PongFX extends Application {
 
 	public static void main(String[] args) {
 		Application.launch(args);
